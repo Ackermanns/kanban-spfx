5e31b4ec842b6a640bd4167fb2e83fa34926ef75
Simon Ackermann <122238891+Ackermanns@users.noreply.github.com>
2024-09-26 22:06:44 +1200

    add: Initialized project

---
+++ src/webparts/kanbanSpfx/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IKanbanSpfxWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'KanbanSpfxWebPartStrings' {
+  const strings: IKanbanSpfxWebPartStrings;
+  export = strings;
+}
